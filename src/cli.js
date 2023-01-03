58943f789d70cb4b4c4c012998f2906b470a7624
Yonas G <yonashailug@gmail.com>
2023-01-02 20:13:20 -0800

    refactor: arrange option props order

--- src/cli.js
+++ src/cli.js
@@ -22,17 +22,19 @@ yargs(process.argv)
   .option('model', {
     type: 'string',
     default: 'text-davinci-003',
-    alias: 'm',
-    desc: 'Show completion model'
+    desc: 'Show completion model',
+    alias: 'm'
   })
   .option('temperature', {
     type: 'number',
     default: 0.5,
-    desc: 'What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.'
+    desc: 'What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.',
+    alias: 't'
   })
   .option('maxTokens', {
     type: 'number',
     default: 600,
-    desc: 'The maximum number of tokens to generate in the completion.'
+    desc: 'The maximum number of tokens to generate in the completion.',
+    alias: 's'
   })
   .parse()
